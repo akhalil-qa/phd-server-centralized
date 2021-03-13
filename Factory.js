e4051547fa927963862b9f1afde769d12f915fd8
Ahmed <alanabi20@hotmail.com>
2021-03-13 06:09:49 +0300

    enforce restrictions for testing

--- Factory.js
+++ Factory.js
@@ -55,7 +55,7 @@ function Factory() {
             spaceAuthority2.enforceRestriction("Space2", "CAMERA", "com.snapchat.android");
             spaceAuthority2.enforceRestriction("Space2", "READ_EXTERNAL_STORAGE", "com.twitter.android");
             spaceAuthority2.enforceRestriction("Space2", "WRITE_EXTERNAL_STORAGE", "com.twitter.android");
-            spaceAuthority2.enforceRestriction("Space2", "CAMERA", "com.photo.hd.camera");
+            spaceAuthority2.enforceRestriction("Space2", "CAMERA", "tools.photo.hd.camera");
             spaceAuthority2.enforceRestriction("Space2", "RECORD_AUDIO", "com.skype.raider");
 
 
