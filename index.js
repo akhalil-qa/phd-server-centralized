ae314f79e5d4a0f0b2bcdfe072037a2d5d14cf48
Ahmed <alanabi20@hotmail.com>
2021-03-25 23:50:28 +0300

    New server implementation with centralized database

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const CertificateAuthorityRecord = mongoose.model("CertificateAuthorityRecord", 
 
 
 //deleteAuthorities();
-populateDatabase();
+//populateDatabase();
 
 // server keys
 var keyPair = {};
