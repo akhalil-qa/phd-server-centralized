0a60a4314231d75b774d816faee6f17f18605474
unknown <AmanAdmin@AKhalil-Laptop.QFPSR.ORG.QA>
2021-05-14 09:25:37 +0300

    update delegated space to be added to delegator's space list

--- index.js
+++ index.js
@@ -503,7 +503,7 @@ app.get("/debug/generateKeyPair", cors(), (req, res) => {
 
 // sign signature
 app.get("/debug/sign/:message/:privateKey", cors(), (req, res) => {
-    console.log("TODO: inside app.get(/debug/sign/:message/:privateKey");
+    console.log("TODO: inside app.get(/debug/sign/:message/:privateKey)");
     try {
         res.send(Crypto.Rsa.sign(req.params.message, req.params.privateKey));
     } catch (e) {
